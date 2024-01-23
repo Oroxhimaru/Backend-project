6c32e6a05e29090d365a1affbfd1a5f5c5ae711e
Oroxhimaru <hm51562@gmail.com>
2024-01-23 23:28:43 +0500

    utility work

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 // require('dotenv').config({path: './env'})  //it is fine totally
 import dotenv from "dotenv" //for using this go to package and add experimental -r dotenv/config --experimental-json-modules
 import connectDB from "./db/index.js";
-
+import {app} from './app.js'
 
 dotenv.config({
     path: './.env'  
 })
 
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
